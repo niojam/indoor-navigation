dc2d694679b5ee80b8b85aa4d9d31bfbd71bd518
Nikita Ojamäe <niojam@ttu.ee>
2020-06-01 19:20:25 +0300

    Build gradle cleaned

--- src/test/java/ee/wemakesoftware/navigation/api/MobileStationControllerTest.java
+++ src/test/java/ee/wemakesoftware/navigation/api/MobileStationControllerTest.java
@@ -21,7 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
